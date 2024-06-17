e70153653f64f4ef81a397feddef3ea645b4f7b4
Stephen Reynolds <mail@stephenreynolds.dev>
2024-06-17 13:33:52 -0400

    cli(gh): disable gh

--- modules/cli/gh.nix
+++ modules/cli/gh.nix
@@ -1,24 +1,17 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkEnableOption mkOption mkIf types optionalString;
+  inherit (lib) mkEnableOption mkIf optionalString;
   cfg = config.my.cli.gh;
 in
 {
   options.my.cli.gh = {
     enable = mkEnableOption "Enable GitHub CLI";
-    extensions = {
-      markdown-preview = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Enable markdown previewer";
-      };
-    };
   };
 
   config = mkIf cfg.enable {
     programs.gh = {
-      enable = true;
+      enable = false;
       settings = {
         editor = optionalString config.programs.neovim.enable "nvim";
         git_protocol = "ssh";
